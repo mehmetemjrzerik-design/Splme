4d0b0a0e01913b2c27e1ed1e63b9d771c378e36c
Mehmet <mehmetemjrzerik@example.com>
2026-05-09 20:08:00 +0300

    fix: next config reactCompiler enabled

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* Turbopack veya React Compiler ayarları varsa burada false yapın */
   experimental: {
-    // reactCompiler: false, // Eğer hata devam ederse bunu ekleyin
-  }
+    reactCompiler: true,
+  },
 };
 
 export default nextConfig;
